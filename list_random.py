a45d84580663f1573c0934cabc9e18705d2c9a6a
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-07-25 21:12:23 +0300

    lesson list and  random module

---
+++ list_random.py
@@ -0,0 +1,20 @@
+#https://docs.python.org/3/library/random.htmlx 
+import random
+num1 = random.randint(1, 10)
+print(num1)
+
+num2 = random.random()
+print(num2)
+
+# Task create a head and  tails coin flip based on random number
+coin_num = random.randint(0, 1)
+if coin_num ==0:
+    print("Heads")
+else:
+    print("Tails")
+# Task Completed 
+#===================
+
+# List 
+#=============
+# https://docs.python.org/3/tutorial/datastructures.html
